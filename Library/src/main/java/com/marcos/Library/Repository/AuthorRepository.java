7580d6de5ae5ade1520533b50b8ea009c79f75ae
Marcos Venicio <marcos.venicio1230@hotmail.com>
2024-06-19 21:08:15 -0300

    Creating Repositorys and add configuration in application.properties

---
+++ Library/src/main/java/com/marcos/Library/Repository/AuthorRepository.java
@@ -0,0 +1,11 @@
+package com.marcos.Library.Repository;
+
+import com.marcos.Library.Models.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Integer> {    
+}
---
+++ Library/src/main/java/com/marcos/Library/Repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.marcos.Library.Repository;
+
+import com.marcos.Library.Models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+}
